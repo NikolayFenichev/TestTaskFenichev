fcbcfe0555b810e1b09250289079d0bfe85c922d
NikolayFenichev <nikolay.fenichev@gmail.com>
2021-04-29 12:56:14 +0300

    Чистка контекста

--- TestTask.DAL/RestaurantManagementContext.cs
+++ TestTask.DAL/RestaurantManagementContext.cs
@@ -5,13 +5,11 @@ namespace TestTask.DAL
 {
     public class RestaurantManagementContext: DbContext
     {
-        private readonly DbContextOptions _options;
         public DbSet<City> Cities { get; set; }
         public DbSet<Restaurant> Restaurants { get; set; }
 
         public RestaurantManagementContext(DbContextOptions options) : base(options)
         {
-            _options = options;
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
